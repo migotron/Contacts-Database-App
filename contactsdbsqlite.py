66baab3a92fcf32b5a7873252762d87451055b88
Miguel Ibarra <40610736+migotron@users.noreply.github.com>
2023-04-21 19:26:14 -0500

    Added connection to SQLite3 database.

--- contactsdbsqlite.py
+++ contactsdbsqlite.py
@@ -1,29 +1,55 @@
+import os
+import sqlite3
+
+DATABASE_NAME = "contacts.db"
+
+# Check if contacts.db exists
+if not os.path.exists(DATABASE_NAME):
+    # Create database file and table
+    conn = sqlite3.connect(DATABASE_NAME)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE contacts 
+                 (id INTEGER PRIMARY KEY,
+                  first_name TEXT,
+                  last_name TEXT,
+                  phone_number TEXT,
+                  email TEXT)''')
+    c.close()
+
+# Connect to database
+conn = sqlite3.connect(DATABASE_NAME)
+c = conn.cursor()
+
 def add_contact():
 # Function to add contact to the database
-    print('Add a new contact...')
+    print("Add a new contact...")
+
 
 def update_contact():
 # Function to update a contact to the database
-    print('Update a contact...')
+    print("Update a contact...")
+
 
 def delete_contact():
 # Function to delete a contact to the database
-    print('Delete a contact...')
+    print("Delete a contact...")
+
 
 def view_all_contacts():
 # Function to view all contacts to the database
-    print('View all contacts...')
+    print("View all contacts...")
+
 
 def main():
     while True:
-        print("Welcome to the contacts database!")
+        print("Welcome to the contacts database app!")
         print("1.) Add a new contact")
         print("2.) Update an existing contact")
         print("3.) Delete a contact")
         print("4.) View all contacts")
         print("5.) Exit program")
 
-        user_choice = int(input("Enter choice: "))
+        user_choice = input("Enter choice: ")
 
         if user_choice == 1:
             add_contact()
